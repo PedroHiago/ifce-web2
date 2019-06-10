7b96b2c56d7180b3b61c9bda7b084f636c37c2e8
PedroHiago <pedrohiagod@gmail.com>
2019-06-10 19:30:06 -0300

    add minhas_reservas user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,15 +10,37 @@
                     <div class="panel-body">
                         <div class="d-flex">
                             <div class="p-2">
-                                <label class="mt-5" for="data_reserva">Listar Reservas</label>
+                                <label class="mt-5" for="data_reserva">Minhas Reservas</label>
                             </div>
                             <div class="ml-auto p-2">
                                 <a href="/user/create" class="mb-5 btn btn-primary">Nova Reserva</a>
                             </div>
                         </div>
+
+                        <table class=" table table-striped table-hover">
+
+                                    <thead>
+                                    <tr>
+                                        <th width="200">Sala</th>
+                                        <th width="200">Status</th>
+                                        <th width="200">Reservada por</th>
+                                    
+                                    </tr>
+                                    </thead>
+
+                                    <tr>
+                                        <th width="200"><input type="text" readonly class="form-control" id="name" placeholder="Nome da Sala" value="Sala01"></th>
+                                        <th width="200"><input type="text" readonly class="form-control" id="name" placeholder="Disponível" value=""></th>
+                                        <th width="200"><input type="text" readonly class="form-control" id="name" placeholder="Ninguém" value=""></th>
+                                        
+                                    
+                                    </tr>
+                                    
+                                    
+                                </table>
                                  
                         <div class="form-group col-12 row">
-
+                        <label class="mt-5" for="data_reserva">Buscar Reservas Disponíveis</label>
                                 <input type="date" 
                                 value="<?php echo date('d-m-y'); ?>" class="form-control" name="data_reserva" 
                                 min="<?php echo $today = date("Y-m-d"); ?>" 
@@ -28,7 +50,7 @@
                                 <button class="btn btn-success btn-lg btn-block">Buscar</button>
                                 
                                 
-                                <table class="mt-5 table table-striped table-hover">
+                                <table class=" table table-striped table-hover">
 
                                     <thead>
                                     <tr>
